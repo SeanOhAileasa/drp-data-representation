109cee118f6b2bde3d7fc89108b0e500d848e0db
SeanOhAileasa <seanohaileasa@protonmail.com>
2021-08-03 09:34:49 +0100

    update repository ./drp-data-representation - The app-server is up and running with python module flask. Specified the RESTful API and now going to use the app-server to perform CRUD operations.

--- rc/server/simpleServer.py
+++ rc/server/simpleServer.py
@@ -1,8 +1,28 @@
 from flask import Flask
-app=Flask(__name__,static_url_path="",static_folder="../../rc/static/")
-@app.route("/")
+app=Flask(import_name=__name__,static_url_path="",static_folder="../../rc/static/")
+@app.route(rule="/")
 def fIndex():
     return "Flask App"
 if __name__=="__main__":
     app.run(debug=True)
     
+cars=[
+    {
+        "reg":"181 G 1234",
+        "make":"Ford",
+        "model":"Modeo",
+        "price":18000
+    },
+    {
+        "reg":"11 MO 1234",
+        "make":"Nissan",
+        "model":"Almera",
+        "price":8000
+    },
+    {
+        "reg":"test",
+        "make":"Nissan",
+        "model":"Almera",
+        "price":8000
+    }
+]
