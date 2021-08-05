35a6e71ce002e34c0804a9dbd1cfd2996d54871c
SeanOhAileasa <seanohaileasa@protonmail.com>
2021-08-05 08:10:54 +0100

    update repository ./drp-data-representation - CRUD (flask.json.jsonify).

--- rc/server/simpleServer.py
+++ rc/server/simpleServer.py
@@ -1,11 +1,28 @@
 from flask import Flask
 app=Flask(import_name=__name__,static_url_path="",static_folder="../../rc/static/")
-@app.route(rule="/")
+# repository ./drp-data-representation
+@app.route(rule="/",methods=["GET"])
 def fIndex():
+    """URL map for "/".
+
+Input: 
+Process: 
+Output: string
+"""    
     return "Flask App"
-if __name__=="__main__":
-    app.run(debug=True)
-    
+# --- END ---
+# repository ./drp-data-representation
+@app.route(rule="/get-all-cars/")
+def fGetAllCars():
+    """URL map for "/get-all-cars/".
+
+Input: 
+Process: (flask.json.jsonify)
+Output: all cars
+"""
+    from flask.json import jsonify
+    return jsonify({"allCars":cars})
+# --- END ---
 cars=[
     {
         "reg":"181 G 1234",
@@ -26,3 +43,5 @@ cars=[
         "price":8000
     }
 ]
+if __name__=="__main__":
+    app.run(debug=True)
